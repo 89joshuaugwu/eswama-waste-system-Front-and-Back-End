dda611ed0c0b15a35c1eecebea181585060b6c8b
89joshuaugwu <joshuaugwu89@gmail.com>
2026-08-28 21:04:38 +0100

    feat: implement MapView component with React Leaflet integration and marker support

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -36,10 +36,9 @@ export default function MapView({ markers = [], center, zoom = 13, onSelectLocat
       className="h-full w-full rounded-lg"
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-        subdomains="abcd"
-        maxZoom={20}
+        attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+        maxZoom={19}
       />
       {onSelectLocation && <ClickToSetLocation onSelect={onSelectLocation} />}
       {markers.map((m) => (
